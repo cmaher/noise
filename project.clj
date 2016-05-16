fbdf2706629c9dafc471f784dd883d60fcd73cd0
Christian Maher <maher.cs@gmail.com>
2016-05-16 00:07:54 -0400

    Dramatically improve the speed of noise generation
    It turns out that understanding clojure enables the writing of better
    clojure

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://opensource.org/licenses/MIT"}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
--- src/clojure/noise/core.clj
+++ src/clojure/noise/core.clj
@@ -1,42 +1,42 @@
 (ns noise.core
-  (:require [clojure.math.numeric-tower :as math])
-  (:import (org.kdotjpg.noise OpenSimplexNoise)
-           (java.util Date)))
+  (:import (org.kdotjpg.noise OpenSimplexNoise)))
 
-(set! *warn-on-reflection* true)
-
-(defn noise-gen
-  ([] (noise-gen (.getTime (Date.))))
+(defn new-noise-gen
+  "Returns an OpenSimplexNoise generator with the given seed"
   ([^long seed] (OpenSimplexNoise. seed)))
 
-(defmacro simplex-noise-base
-  ([noise-gen & coords]
-    `(.eval ~noise-gen ~@coords)))
-
 (defn simplex
-  ([noise-gen x y] (simplex-noise-base noise-gen x y))
-  ([noise-gen x y z] (simplex-noise-base noise-gen x y z))
-  ([noise-gen x y z w] (simplex-noise-base noise-gen x y z w)))
+  "Returns the 2d, 3d, or 4d noise of the given coordinates.
+  The returned noise will be in the range (-1 < noise < 1)"
+  ([^OpenSimplexNoise noise-gen x y] (.eval noise-gen x y))
+  ([^OpenSimplexNoise noise-gen x y z] (.eval noise-gen x y z))
+  ([^OpenSimplexNoise noise-gen x y z w] (.eval noise-gen x y z w)))
 
 (defn scale
+  "Given a noise value in the range (-1 < noise < 1),
+  return the noise scaled to the new range (low <= scaled-noise <= high)"
   [noise low high]
   (let [straddle-result (/ (* noise (- high low)) 2)
         adjustment (/ (+ high low) 2)]
     (+ straddle-result adjustment)))
 
 (defn fbm
-  "noise subjected to fractal Brownian motion"
+  "Fractal Brownian Motion applies successive iterations of noise at varying amounts of influence.
+  Resulting in a more textured noise"
   [noise-gen iterations persistence scale coords]
-  (let [iters (range 0 iterations)
-        amps (mapv #(math/expt persistence %) iters)
-        max-amp (reduce + amps)
-        freqs (mapv #(* scale (math/expt 2 %)) iters)
-        make-noise (fn [i]
-                     (let [freq (get freqs i)
-                           amp (get amps i)
-                           scaled-coords (map #(* freq %) coords)
-                           iter-noise (apply simplex noise-gen scaled-coords)]
-                       (* iter-noise amp)))
-        noises (map make-noise iters)
-        noise (reduce + noises)]
-    (/ noise max-amp)))
+  (loop [iter 0
+         max-amp 0.0
+         amp 1.0
+         freq scale
+         noise 0.0]
+    (if (< iter iterations)
+      (let [scaled-coords (map #(* freq %) coords)
+            iter-noise (apply simplex noise-gen scaled-coords)
+            scaled-noise (* amp iter-noise)]
+        (recur (inc iter)
+               (+ amp max-amp)
+               (* amp persistence)
+               (* freq 2)
+               (+ noise scaled-noise)))
+      (/ noise max-amp))))
+
